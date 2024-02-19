57ef7c6843af17b0205f9a99f73235c15f33b2f7
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2024-02-19 15:49:01 -0800

    Create a config file

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+//site name
+define('SITE_NAME', 'RideConnect');
+
+
+//App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', '/');
+define('URL_SUBFOLDER', '');
+
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'renu');
+define('DB_PASS', 'grenupillai91');
+define('DB_NAME', 'rideconnect');
